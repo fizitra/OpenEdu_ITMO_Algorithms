2c27fe1f45baa36ed529a3046c5747cafc8e29ba
fizitra <fizitra@gmail.com>
2016-09-28 11:28:00 +0300

    Задача 4 верное решение

---
+++ list_generator.py
@@ -0,0 +1,14 @@
+from random import randint, random
+
+def list_generator(N, k, precision):
+    new_list = [str(round(random()*randint(0, k),precision)) for i in range(N)]
+    return new_list
+
+if __name__ == '__main__':
+    N = int(input())
+    k = int(input())
+    precision = int(input())
+    name_of_file = input()
+    file = open(name_of_file+'.in','w')
+    file.write(str(N)+'\n'+' '.join(list_generator(N, k, precision))+'\n')
+    file.close()
--- week1_problem4_sortland.py
+++ week1_problem4_sortland.py
@@ -1,10 +1,34 @@
+'''
+В первом варианте программы предполагалось создание словаря с индексами
+всех элементов, операция в данной задаче не нужная, т.к. список по сути
+есть словарь с ключами равными омеру индекса, кроме того, как оказалось
+при тестировании, операция исключительно затратная по времени print('2')
+'''
+#все
+#from time import time
+#start = time()
+
 file = open('sortland.in', 'r')
 input_list = [line.strip() for line in file]
 file.close()
+
+#print('1',time()-start)
+#start = time()
+
 #prinst(input_list)
-sortland_list = {list(map(float,input_list[1].split()))[i-1]:i for i in range(1,int(input_list[0])+1)}
-sortland_sort = sorted(list(map(float,input_list[1].split())))
+#sortland_list = {list(map(float,input_list[1].split()))[i-1]:i for i in range(1,int(input_list[0])+1)}
+#print('2',time()-start)
+#start = time()
+
+sortland_list = list(map(float,input_list[1].split()))
+sortland_sort = sorted(sortland_list)
+
+#print('3',time()-start)
+#start = time()
 
 file = open('sortland.out','w')
-file.write(' '.join([str(x) for x in [sortland_list[sortland_sort[0]],sortland_list[sortland_sort[int(input_list[0])//2]],sortland_list[sortland_sort[-1]]]])+'\n')
+#file.write(' '.join([str(x) for x in [sortland_list[sortland_sort[0]],sortland_list[sortland_sort[int(input_list[0])//2]],sortland_list[sortland_sort[-1]]]])+'\n')
+file.write("{:} {:} {:}\n".format(sortland_list.index(sortland_sort[0])+1,sortland_list.index(sortland_sort[int(input_list[0])//2])+1,sortland_list.index(sortland_sort[-1])+1))
 file.close()
+
+#print('4',time()-start)
